d90d9ad7a8c370a585bab643e6a886ae709fc4b3
GSTSPower <120174653+GSTSPower@users.noreply.github.com>
2023-01-29 15:56:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const left = document.querySelector('.arrow-left')
 const right = document.querySelector('.arrow-right')
 const container = document.querySelector('.slider-container')
-let slides = document.getElementsByClassName("slide")
+let slides = document.getElementsByClassName("slide");
 
 
 // let offset = 0;
@@ -42,7 +42,7 @@ function currentSlide(n) {
 
 
 function showSlides(n) {
-    
+    // let slides = document.getElementsByClassName("slide");
     if (n > slides.length) {
       slideIndex = 1;
     }
@@ -62,7 +62,7 @@ function showSlides(n) {
     timer = setInterval(function(){
       slideIndex++;
       showSlides(slideIndex);
-    },7000);
+    },5000);
   }
   
   
